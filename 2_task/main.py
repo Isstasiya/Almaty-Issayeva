93333fb97ec00ddda85108043c5160f38df27513
stasiya.issayeva <stasiy.issayeva@gmail.com>
2020-11-24 11:35:08 +0600

    + Add folder

---
+++ 2_task/main.py
@@ -0,0 +1,39 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.nn)
+        self.show()
+        self.t = False
+
+    def nn(self):
+        self.t = True
+
+    def paintEvent(self, event):
+        if self.t:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawRect(30, 30, 120, 30)
+        qp.setBrush(QColor(0, 255, 0))
+        qp.drawRect(30, 60, 120, 30)
+        qp.setBrush(QColor(0, 0, 255))
+        qp.drawRect(30, 90, 120, 30)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
